bcac0baab1b9648a679e153eb85d540d3d256026
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-15 19:00:57 -0300

    update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -139,7 +139,11 @@ class MainMenuState extends MusicBeatState
 		}
 		#end
 
-		super.create();
+		#if android
+    addVirtualPad(LEFT_FULL, A_B);
+    #end
+	 
+	  super.create();
 	}
 
 	#if ACHIEVEMENTS_ALLOWED
