5b90574dda46ae73deebbfd85a7a693b5c0c585f
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-15 18:59:40 -0300

    update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -170,6 +170,9 @@ class FreeplayState extends MusicBeatState
 		text.setFormat(Paths.font("vcr.ttf"), 18, FlxColor.WHITE, RIGHT);
 		text.scrollFactor.set();
 		add(text);
+		#if android
+    addVirtualPad(LEFT_FULL, A_B);
+    #end
 		super.create();
 		trace(songs);
 		for (i in songs) {
