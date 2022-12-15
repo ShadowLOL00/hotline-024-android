84a7287a5f88d225f4d9f589244164455b10b630
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-15 15:47:14 -0300

    update MenuCharacter.hx

--- source/MenuCharacter.hx
+++ source/MenuCharacter.hx
@@ -22,36 +22,36 @@ class MenuCharacter extends FlxSprite
 
 		switch(character) {
 			case 'bf':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_BF');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_BF');
 				animation.addByPrefix('idle', "M BF Idle", 24);
 				animation.addByPrefix('confirm', 'M bf HEY', 24, false);
 
 			case 'gf':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_GF');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_GF');
 				animation.addByPrefix('idle', "M GF Idle", 24);
 
 			case 'dad':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Dad');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_Dad');
 				animation.addByPrefix('idle', "M Dad Idle", 24);
 
 			case 'spooky':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Spooky_Kids');
+				frames = SUtil.getPath() + SUtil.getPath() +  Paths.getSparrowAtlas('menucharacters/Menu_Spooky_Kids');
 				animation.addByPrefix('idle', "M Spooky Kids Idle", 24);
 
 			case 'pico':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Pico');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_Pico');
 				animation.addByPrefix('idle', "M Pico Idle", 24);
 
 			case 'mom':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Mom');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_Mom');
 				animation.addByPrefix('idle', "M Mom Idle", 24);
 
 			case 'parents-christmas':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Parents');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_Parents');
 				animation.addByPrefix('idle', "M Parents Idle", 24);
 
 			case 'senpai':
-				frames = Paths.getSparrowAtlas('menucharacters/Menu_Senpai');
+				frames = SUtil.getPath() + Paths.getSparrowAtlas('menucharacters/Menu_Senpai');
 				animation.addByPrefix('idle', "M Senpai Idle", 24);
 		}
 		animation.play('idle');
