891756089b365984293ec61f46efbdfe9ae5a771
ShadowLOL00 <95530213+ShadowLOL00@users.noreply.github.com>
2022-12-15 16:25:10 -0300

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -83,6 +83,11 @@ class TitleState extends MusicBeatState
 		// DEBUG BULLSHIT
 
 		swagShader = new ColorSwap();
+		
+		#if android
+    addVirtualPad(LEFT_FULL, A_B);
+    #end
+		
 		super.create();
 
 		FlxG.save.bind('funkin', 'ninjamuffin99');
